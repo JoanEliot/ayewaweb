e92a84616130ca2f5cc0c9f79d14a8b3a6034ce4
Peter D Bethke <pdbethke@siteshell.net>
2020-10-24 17:26:08 -0400

    Updating search

--- ayewaweb/settings/base.py
+++ ayewaweb/settings/base.py
@@ -105,15 +105,14 @@ WSGI_APPLICATION = 'ayewaweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- ayewaweb/settings/dev.py
+++ ayewaweb/settings/dev.py
@@ -10,7 +10,16 @@ SECRET_KEY = '&6vr^v=yg0^$-3sts^+e26-**ae1(u@37yu@w0u%l1bco^n8d8'
 ALLOWED_HOSTS = ['*'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dav529ipcka40a',
+        'USER': 'ynowdoifamdvvj',
+        'PASSWORD': 'f3cfd55e7fb521fd57e9ec7eab6f8b6e522b7ddf7809fdf7224864bd139f9071',
+        'HOST': 'ec2-35-169-92-231.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 try:
     from .local import *
